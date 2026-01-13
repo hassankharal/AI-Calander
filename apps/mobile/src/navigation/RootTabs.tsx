76d9c254039c479ea355a8b625848873effee0f9
Kharalh <81539151+hassankharal@users.noreply.github.com>
2026-01-12 19:25:24 -0600

    P2-S1: add tab navigation and screen scaffolds

---
+++ apps/mobile/src/navigation/RootTabs.tsx
@@ -0,0 +1,28 @@
+﻿import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import CalendarScreen from '../screens/CalendarScreen';
+import SchedulerScreen from '../screens/SchedulerScreen';
+import TasksScreen from '../screens/TasksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+export type RootTabParamList = {
+  Home: undefined;
+  Calendar: undefined;
+  Scheduler: undefined;
+  Tasks: undefined;
+  Settings: undefined;
+};
+
+const Tab = createBottomTabNavigator<RootTabParamList>();
+
+export default function RootTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Calendar" component={CalendarScreen} />
+      <Tab.Screen name="Scheduler" component={SchedulerScreen} />
+      <Tab.Screen name="Tasks" component={TasksScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
